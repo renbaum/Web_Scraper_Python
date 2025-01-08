26bd7b0bcbf90e2717268b63d024c77f71ff4318
renbaum <renbaum04@bluewin.ch>
2025-01-08 15:05:57 +0100

    Stage 3: What the File?

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -7,18 +7,28 @@ if __name__ == "__main__":
     # url = "https://www.nature.com/articles/d41586-023-00103-3"
 
     try:
-        if "nature.com" not in url: raise Exception("Invalid page!")
-        headers = {'Accept': 'application/json'}
-        response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        #if "nature.com" not in url: raise Exception("Invalid page!")
+        response = requests.get(url)
         if(response.status_code != 200):
-            raise Exception("Invalid page!")
-        soup = BeautifulSoup(response.text, 'html.parser')
-        titel = soup.find('title')
-        title = titel.text
-        description = soup.find('meta', attrs={"name": "description"})['content']
+            raise Exception(f"The URL returned {response.status_code}!")
 
-        dict = {'title': title, 'description': description}
-        print(dict)
+        headers = {'Accept': 'application/json'}
+        # response = requests.get(url).content
+
+        #page_content = requests.get(url).content
+
+        with open("source.html", "wb")  as file:
+            file.write(response.content)
+
+        print("Content saved.")
+
+#        soup = BeautifulSoup(response.text, 'html.parser')
+#        titel = soup.find('title')
+#        title = titel.text
+#        description = soup.find('meta', attrs={"name": "description"})['content']
+
+#        dict = {'title': title, 'description': description}
+#        print(dict)
 
     except Exception as e:
         print(e)
